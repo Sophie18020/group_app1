9ac82e38866691c7d278b66786890c1c87860206
VeronicaDem <veron.dem4enko2014@yandex.ru>
2020-01-12 23:01:10 +0300

    Updates

---
+++ precache-manifest.f120fda29e1af399ba34ee0d8bb47153.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc61da47be76f452fb2c8849e1547a2",
+    "url": "/group-app1/index.html"
+  },
+  {
+    "revision": "39af09640271904e70b9",
+    "url": "/group-app1/static/js/2.542960b1.chunk.js"
+  },
+  {
+    "revision": "a045b6012a989d8e3df1",
+    "url": "/group-app1/static/js/main.5f8317b8.chunk.js"
+  },
+  {
+    "revision": "8e6a31f2a4d81e9b1146",
+    "url": "/group-app1/static/js/runtime~main.d47228df.js"
+  }
+]);
